4aeb33e398cebb7747ae8bdfa652bf13356d88e9
anri varshanidze <anrivarshanidze2407@gmail.com>
2023-11-29 15:30:18 +0400

    change readme

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(CustomerController::class)->group(function () {
     Route::get('/', 'index')->name('main.index');
-    Route::get('about', 'about')->name('about');
+    Route::get('main/about', 'about')->name('about');
     Route::get('contact', 'contact')->name('contact');
     Route::get('portfolio', 'portfolio')->name('portfolio');
     Route::get('comment', 'comment')->name('comment');
